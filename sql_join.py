6e4ae4ea71f5b6d6697f03447a93fcecc2a1ec37
se000ra <se000ra.gmail.com>
2014-10-25 14:38:05 +0600

    sql  join added

---
+++ sql_join.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2014 van <van@vanleno>
+#
+# Distributed under terms of the MIT license.
+
+"""
+
+"""
+import sqlite3
+
+with sqlite3.connect('new.db') as connection:
+    c = connection.cursor()
+    
+    c.execute("""
+            SELECT population.city, population.population, regions.region
+            FROM population, regions
+            WHERE population.city = regions.city
+            """)
+
+
+
+    rows = c.fetchall()
+    for r in rows:
+        print r[0], r[1], r[2]
+        
