a818d58e670bdefb49e34f0622548f981a2510ba
se000ra <se000ra.gmail.com>
2014-10-25 13:27:12 +0600

    sql with added

--- sqlb.py
+++ sqlb.py
@@ -11,12 +11,12 @@
 """
 
 import sqlite3
-
 conn = sqlite3.connect('new.db')
 cursor = conn.cursor()
-cursor.execute("INSERT INTO population VALUES('New York City', NY, 8200000)")
-cursor.execute("INSERT INTO population VALUES('New York City', "NY", 8200000)")
-cursor.execute("INSERT INTO population VALUES('New York City', 'NY', 8200000)")
-cursor.execute("INSERT INTO population VALUES('San Francisco', 'CA', 800000)")
-conn.commit()
+try:
+    cursor.execute("INSERT INTO population VALUES('New York City', 'NY', 8200000)")
+    cursor.execute("INSERT INTO pppppopulation VALUES('San Francisco', 'CA', 800000)")
+    conn.commit()
+except sqlite3.OperationalError:
+    print "Oops!"
 conn.close()
